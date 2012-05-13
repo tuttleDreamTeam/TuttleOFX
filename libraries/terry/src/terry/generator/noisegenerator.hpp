95c64f834252ed4070710e16127c72b7b35f8ec3
Adam <adam@G51Overboost.(none)>
2012-05-13 18:09:54 +0200

    terry number3

--- libraries/terry/src/terry/generator/noisegenerator.hpp
+++ libraries/terry/src/terry/generator/noisegenerator.hpp
@@ -32,32 +32,45 @@ struct NoiseGeneratorFunctor
 	typedef reference result_type;
 	BOOST_STATIC_CONSTANT( bool, is_mutable = false );
 
-	value_type _in_color;
+	
 	float      _sigma;
 	float	   _nu;
-	
+	float	   _color;
+	float	   _r_weight;
+	float	   _g_weight;
+	float	   _b_weight;
+	float	   _bw_weight;
+	float	   _nb;
 	
 
 
 	NoiseGeneratorFunctor() {}
-	NoiseGeneratorFunctor(  float sigma, float nu ) //set maybe an enum for
-		:  _sigma(sigma), _nu(nu){}
+	NoiseGeneratorFunctor(  float sigma, float nu, int color, float r_weight, float	   g_weight, float	b_weight, float bw_weight, float nb) //set maybe an enum for
+		:  _sigma(sigma), _nu(nu), _color(color), _r_weight(r_weight), _g_weight(g_weight), _b_weight(b_weight), _bw_weight(bw_weight), _nb(nb){}
 
 	result_type operator()( const point_t& p ) const
 	{
                 boost::random::random_device gen;
-		
-		
                 boost::random::normal_distribution<> dist(_nu, _sigma);
 		//boost::random::uniform_int_distribution<> dist(0, 255);
-		// color gray
+		/*// color gray
                 value_type color;
-		double a;
-		a = 0.004*dist(gen);
-                color_convert( rgba32f_pixel_t( a, a, a, 1 ), color);
-
+		double BW, nb ,r,g,b;
+		
+		BW = dist(gen) * _bw_weight + nb ( 1 - _bw_weight );
+                color_convert( rgba32f_pixel_t( BW, BW, BW, 1 ), color);*/
+		
 		//color rgba
-		//color_convert( rgba32f_pixel_t( a, a, a, 1 ), color);
+		value_type color;
+		double nb,r,g,b;
+		
+		nb = dist(gen);
+		r = dist(gen) * _r_weight + nb * ( 1 - _bw_weight );
+		g = dist(gen) * _g_weight + nb * ( 1 - _bw_weight );
+		b = dist(gen) * _b_weight + nb * ( 1 - _bw_weight );
+
+		color_convert( rgba32f_pixel_t(r,g, b, 1 ), color);
+	
                 return color;
 	}
 
